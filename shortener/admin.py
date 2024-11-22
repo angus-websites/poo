08f742c8bd0314c1a52173ec17a19480db51cd78
Angus <angus.goody@gmail.com>
2024-11-22 15:30:42 +0000

    updated admin ui

--- shortener/admin.py
+++ shortener/admin.py
@@ -7,7 +7,9 @@ from shortener.models import URL
 # Register the model with the admin site
 @admin.register(URL)
 class ShortenedURLAdmin(admin.ModelAdmin):
-    # Optional: Customize the admin list display
-    list_display = ('original_url', 'short_code', 'created_at')
+    # Display the new fields in the admin list view
+    list_display = ('original_url', 'short_code', 'created_at', 'clicks', 'last_accessed')
     search_fields = ('original_url', 'short_code')
-    list_filter = ('created_at',)
+    list_filter = ('created_at', 'last_accessed')
+
+    readonly_fields = ('clicks', 'last_accessed')
