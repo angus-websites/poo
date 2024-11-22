d8de37202e3041cb8a8fa4a649f822b7cf5f737d
Angus <angus.goody@gmail.com>
2024-11-22 15:16:54 +0000

    added url admin page

--- shortener/admin.py
+++ shortener/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from shortener.models import URL
+
+
 # Register your models here.
+# Register the model with the admin site
+@admin.register(URL)
+class ShortenedURLAdmin(admin.ModelAdmin):
+    # Optional: Customize the admin list display
+    list_display = ('original_url', 'short_code', 'created_at')
+    search_fields = ('original_url', 'short_code')
+    list_filter = ('created_at',)
